da01887b5eb03816189955a7ab1733a5c2048c21
Tapayoga <48332417+Tapayoga@users.noreply.github.com>
2019-03-08 10:21:45 +0700

    Create tugas1.java

---
+++ tugas1.java
@@ -0,0 +1,8 @@
+public class BRizky {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Rizky Tapayoga");
+        System.out.println("NIM     : 17090123");
+        System.out.println("Kelas   : 4B");
+    }
+    
+}
